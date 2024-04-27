f7ef755fb998415e2a05cbb5d4180cb49d9b8539
unknown <erkmgwr@gmail.com>
2024-04-27 15:26:40 -0300

    feat: adding logic to set current letter

--- src/js/guess-the-word/index.js
+++ src/js/guess-the-word/index.js
@@ -1,23 +1,22 @@
 
-function setup(){
-    const box = document.querySelector('.box');
-    
-    setupFirstCurrentWord(box);
-
-    window.addEventListener('keydown', function(event){
-        console.log(event);
-    });
-}
-
 
 function setupFirstCurrentWord(box){
     const firstWord = getFirstWord(box);
+    const firstLetter = getFirstLetter(firstWord);
+
+    console.log(firstLetter);
 
     setupLettersAsCurrent(firstWord);
-
     setNewCurrentWord(firstWord);
+    setNewCurrentLetter(firstLetter);
 }
 
+function getFirstLetter(word){
+    const letters = word.getElementsByClassName('letter');
+    const firstLetter = letters[0];
+
+    return firstLetter;
+}
 
 function getFirstWord(box){
     const words = box.getElementsByClassName('word');
@@ -38,12 +37,50 @@ function getCurrentWord(){
     return document.querySelector('.current-word');
 }
 
-function getCurrentCell(){}
+function getNextLetterAfterCurrent(){
+    const currentLetter = getCurrentLetter();
+    const nextLetter = currentLetter.nextElementSibling;
+
+    return nextLetter;
+}
+
+function getCurrentLetter(){
+    return document.querySelector('.current-letter');
+}
+
+function setContentOnCurrentLetter(content){
+    const currentLetter = getCurrentLetter();
+    currentLetter.textContent = content;
+}
+
+function handleKeyPress(event){
+    const content = event.key;
+    setContentOnCurrentLetter(content);
+}
+
 
 function setNewCurrentWord(word){
     word.classList.replace('word', 'current-word');
 }
 
-function setNewCurrentCell(){}
+function setNewCurrentLetter(letter){
+    letter.classList.replace('letter', 'current-letter');
+}
+
+function setup(){
+    const box = document.querySelector('.box');
+    
+    setupFirstCurrentWord(box);
+    
+
+    window.addEventListener('keydown', function(event){
+        const currentLetter = getCurrentLetter();
+        setNewCurrentLetter(currentLetter);
+        const nextLetter = getNextLetterAfterCurrent();
+        setNewCurrentLetter(nextLetter);
+        setContentOnCurrentLetter(event.key);
+        console.log("okay dokay");
+    });
+}
 
 setup();
